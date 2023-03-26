4daaaf375b5937a1d60a2c6f20cbca1b997f9d9f
joepriase40 <joepriase40@gmail.com>
2023-03-25 18:00:08 -0700

    Switch

--- 0x03-python-data_structures/12-switch.py
+++ 0x03-python-data_structures/12-switch.py
@@ -1,9 +1,5 @@
 #!/usr/bin/python3
-# 11-delete_at.py
-
-
-def delete_at(my_list=[], idx=0):
-    """Delete an item at a specific position in a list."""
-    if idx >= 0 and idx < len(my_list):
-        del my_list[idx]
-    return (my_list)
+a = 89
+b = 10
+a, b = b, a
+print("a={:d} - b={:d}".format(a, b))
